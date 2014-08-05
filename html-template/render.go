4f07f447ebd89a27f026b383091a8d2bed91e699
funnythingz <funnythingz69+github@gmail.com>
2014-08-05 16:29:17 +0900

    try html/template

---
+++ html-template/render.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "html/template"
+    "net/http"
+)
+
+type Dat struct {
+    Title string
+    Body string
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    funcMap := template.FuncMap{
+        "safehtml": func(text string) template.HTML {
+                        return template.HTML(text)
+                    },
+    }
+
+    templates := template.Must(template.New("").Funcs(funcMap).ParseFiles("templates/layout.html", "templates/view.html"))
+
+    dat := Dat{
+        Title: "ほげやん",
+        Body: "ほげではない。"}
+
+    err := templates.ExecuteTemplate(w, "layout", dat)
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+
+}
+
+func main() {
+    http.HandleFunc("/", viewHandler)
+    http.ListenAndServe(":8888", nil)
+}
