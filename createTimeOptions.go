606aabce606f70a292305f3d10f86b9ca2fdb13d
funnythingz <funnythingz69+github@gmail.com>
2014-08-05 18:46:09 +0900

    Fixed createTimeOptions

---
+++ createTimeOptions.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "reflect"
+)
+
+func createTimeOptions() []string {
+
+    var timeOptions []string
+
+    for i := 0; i < 24; i++ {
+
+        count := fmt.Sprintf("%d", i)
+
+        if reflect.DeepEqual(len(count), 2) {
+            timeOptions = append(timeOptions, count + ":00")
+            timeOptions = append(timeOptions, count + ":30")
+        } else {
+            timeOptions = append(timeOptions, "0" + count + ":00")
+            timeOptions = append(timeOptions, "0" + count + ":30")
+        }
+    }
+
+    return timeOptions
+
+}
+
+func main() {
+    fmt.Println(createTimeOptions())
+}
