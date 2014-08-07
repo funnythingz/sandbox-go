3d23589269765c436f5006b851d5ddc88de37ba5
funnythingz <funnythingz69+github@gmail.com>
2014-08-07 18:27:33 +0900

    Fixed smooth

--- smooth.go
+++ smooth.go
@@ -3,25 +3,27 @@ package main
 import "fmt"
 
 func main() {
-  helloKeitan()
-  goodDayKeitan()
-  smoothLoop()
+    helloKeitan()
+    goodDayKeitan()
+    smoothLoop()
 }
 
 func helloKeitan() {
-  keitan := "こんにちは！けいたん"
-  fmt.Println(keitan)
+    keitan := "こんにちは！けいたん"
+    fmt.Println(keitan)
 }
 
 func goodDayKeitan() {
-  var keitan string
-  keitan = "良い日だね！けいたん"
-  fmt.Println(keitan)
+    keitan := "良い日だね！けいたん"
+    fmt.Println(keitan)
 }
 
 func smoothLoop() {
-  smooth := []string {"keitan", "ipe", "hrtn?"}
-  for i := 0; i < len(smooth); i++ {
-    fmt.Println(smooth[i])
-  }
+
+    smooth := []string {"keitan", "ipe", "hrtn?"}
+
+    for _, v := range smooth {
+        fmt.Println(v)
+    }
+
 }
