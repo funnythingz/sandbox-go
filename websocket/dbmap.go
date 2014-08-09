039d35cc14fab53161346d75e69a256b76614666
funnythingz <funnythingz69+github@gmail.com>
2014-08-09 22:39:06 +0900

    added func DeleteEntry

--- websocket/dbmap.go
+++ websocket/dbmap.go
@@ -1,15 +1,18 @@
 package main
 
-import(
-    "database/sql"
-    "github.com/coopernurse/gorp"
-    _ "github.com/mattn/go-sqlite3"
-    "time"
+import (
+	"database/sql"
+	"github.com/coopernurse/gorp"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"time"
 )
 
 type Entry struct {
-    CreateAt int64
-    Message string
+	CreateAt int64
+	Message  string
 }
 
 var DatabaseFile string
@@ -17,37 +20,45 @@ var Dbmap *gorp.DbMap
 
 func initDB() {
 
-    DatabaseFile = "test.db"
+	DatabaseFile = "test.db"
 
-    db, _ := sql.Open("sqlite3", DatabaseFile)
-    Dbmap = &gorp.DbMap{
-        Db: db,
-        Dialect: gorp.SqliteDialect{},
-    }
+	db, _ := sql.Open("sqlite3", DatabaseFile)
+	Dbmap = &gorp.DbMap{
+		Db:      db,
+		Dialect: gorp.SqliteDialect{},
+	}
 
-    Dbmap.AddTableWithName(Entry{}, "entry").SetKeys(true, "CreateAt")
+	Dbmap.AddTableWithName(Entry{}, "entry").SetKeys(true, "CreateAt")
 
-    Dbmap.DropTables()
-    Dbmap.CreateTables()
+	Dbmap.DropTables()
+	Dbmap.CreateTables()
 
 }
 
 func InsertEntry(m string) {
-    tx, _ := Dbmap.Begin()
-    tx.Insert(&Entry{time.Now().UnixNano(), m})
-    tx.Commit()
+	tx, _ := Dbmap.Begin()
+	tx.Insert(&Entry{time.Now().UnixNano(), m})
+	tx.Commit()
 }
 
-func GetEntryAll() []string {
+func DeleteEntry(params martini.Params, r render.Render) {
+	log.Println(params)
+	id := params["id"]
+	log.Println(id)
+	Dbmap.Exec("delete from Entry where CreateAt=?", id)
+	log.Println(GetEntryAll())
+}
 
-    list, _ := Dbmap.Select(Entry{}, "select * from entry order by CreateAt desc")
+func GetEntryAll() []*Entry {
 
-    var result []string
+	list, _ := Dbmap.Select(Entry{}, "select * from entry order by CreateAt desc")
 
-    for _, val := range list {
-        entry := val.(*Entry)
-        result = append(result, entry.Message)
-    }
+	var result []*Entry
 
-    return result
+	for _, val := range list {
+		entry := val.(*Entry)
+		result = append(result, entry)
+	}
+
+	return result
 }
