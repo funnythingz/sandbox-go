757dc7d17ff27656b21e46c8ba8d7ea91790b6cb
funnythingz <funnythingz69+github@gmail.com>
2014-08-09 22:34:16 +0900

    added delete button event

--- websocket/assets/app.js
+++ websocket/assets/app.js
@@ -7,11 +7,19 @@ var App = function() {
         webSocket.onmessage = function(response) {
 
             console.log(response);
+            var deleteObj = JSON.parse(response.data);
+            console.log(deleteObj);
 
-            var message = $('<li class="list-group-item">').text(response.data);
+            if(_.isEqual(deleteObj.type, 'delete')) {
+                console.log('.list-group-item[data-id="' + deleteObj.data + '"]');
+                var $el = $('.list-group-item[data-id="' + deleteObj.data + '"]');
+                $el.remove();
+            } else {
+                var message = $('<li class="list-group-item">').text(response.data);
 
-            $('#messages').prepend(message);
-            $('#inputMessage').val('');
+                $('#messages').prepend(message);
+                $('#inputMessage').val('');
+            }
         }
 
         $('#form').on('submit', function(e) {
@@ -19,8 +27,30 @@ var App = function() {
             e.preventDefault();
 
             if(! _.isEmpty($('#inputMessage').val())) {
-                webSocket.send($('#inputMessage').val());
+                webSocket.send(JSON.stringify({type: 'new', data: $('#inputMessage').val()}));
             }
+
+        });
+
+        var deleteButton = $('.delete-entry');
+
+        deleteButton.on('click', function(e) {
+
+            e.stopPropagation();
+
+            var data = $(this).data('id');
+            var url = '/ws/' + data;
+
+            var promise = $.ajax({
+                "url" : url,
+                "data" : data,
+                "type" : "DELETE"
+            });
+
+            promise.done(function() {
+                console.log(data);
+                webSocket.send(JSON.stringify({type: 'delete', data: data}));
+            });
 
         });
 
