e4c71021e9299c85639cf09a566949c11a8d7c46
funnythingz <funnythingz69+github@gmail.com>
2014-08-09 22:41:21 +0900

    updated routes

--- websocket/app.go
+++ websocket/app.go
@@ -1,24 +1,25 @@
 package main
 
-import(
-    "github.com/go-martini/martini"
-    "github.com/martini-contrib/render"
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
 )
 
 func main() {
 
-    initDB()
+	initDB()
 
-    m := martini.Classic()
+	m := martini.Classic()
 
-    m.Use(render.Renderer(render.Options{
-        Layout: "layout",
-    }))
-    m.Use(martini.Static("assets"))
+	m.Use(render.Renderer(render.Options{
+		Layout: "layout",
+	}))
+	m.Use(martini.Static("assets"))
 
-    m.Get("/", Index)
-    m.Get("/about", About)
-    m.Get("/ws", WebSocket)
+	m.Get("/", Index)
+	m.Get("/about", About)
+	m.Get("/ws", WebSocket)
+	m.Delete("/ws/:id", DeleteEntry)
 
-    m.Run()
+	m.Run()
 }
--- websocket/websocket.go
+++ websocket/websocket.go
@@ -1,85 +1,96 @@
 package main
 
-import(
-    "log"
-    "net"
-    "net/http"
-    "sync"
+import (
+	"log"
+	"net"
+	"net/http"
+	"reflect"
+	"strings"
+	"sync"
 
-    "github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
+	"github.com/mattn/go-scan"
 )
 
 var ActiveClients = make(map[ClientConn]int)
 var ActiveClientsRWMutex sync.RWMutex
 
 type ClientConn struct {
-
-    websocket *websocket.Conn
-    clientIP net.Addr
+	websocket *websocket.Conn
+	clientIP  net.Addr
 }
 
 func addClient(cc ClientConn) {
 
-    ActiveClientsRWMutex.Lock()
-    ActiveClients[cc] = 0
-    ActiveClientsRWMutex.Unlock()
+	ActiveClientsRWMutex.Lock()
+	ActiveClients[cc] = 0
+	ActiveClientsRWMutex.Unlock()
 }
 
 func deleteClient(cc ClientConn) {
 
-    ActiveClientsRWMutex.Lock()
-    delete(ActiveClients, cc)
-    ActiveClientsRWMutex.Unlock()
+	ActiveClientsRWMutex.Lock()
+	delete(ActiveClients, cc)
+	ActiveClientsRWMutex.Unlock()
 }
 
 func broadcastMessage(messageType int, message []byte) {
 
-    ActiveClientsRWMutex.RLock()
+	ActiveClientsRWMutex.RLock()
 
-    defer ActiveClientsRWMutex.RUnlock()
+	defer ActiveClientsRWMutex.RUnlock()
 
-    for client, _ := range ActiveClients {
-        if err := client.websocket.WriteMessage(messageType, message); err != nil {
-            return
-        }
-    }
+	for client, _ := range ActiveClients {
+		if err := client.websocket.WriteMessage(messageType, message); err != nil {
+			return
+		}
+	}
 }
 
 func WebSocket(w http.ResponseWriter, r *http.Request) {
 
-    ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
 
-    if _, ok := err.(websocket.HandshakeError); ok {
+	if _, ok := err.(websocket.HandshakeError); ok {
 
-        http.Error(w, "Not a websocket handshake", 400)
-        return
+		http.Error(w, "Not a websocket handshake", 400)
+		return
 
-    } else if err != nil {
+	} else if err != nil {
 
-        log.Println(err)
-        return
+		log.Println(err)
+		return
 
-    }
+	}
 
-    client := ws.RemoteAddr()
-    clientConn := ClientConn{ws, client}
-    addClient(clientConn)
+	client := ws.RemoteAddr()
+	clientConn := ClientConn{ws, client}
+	addClient(clientConn)
 
-    for {
-        messageType, p, err := ws.ReadMessage()
+	for {
+		messageType, p, err := ws.ReadMessage()
 
-        if err != nil {
-            deleteClient(clientConn)
-            log.Println(err)
-            return
-        }
+		if err != nil {
+			deleteClient(clientConn)
+			log.Println(err)
+			return
+		}
 
-        broadcastMessage(messageType, p)
-        log.Println(ByteToStr(p))
+		broadcastMessage(messageType, p)
+		log.Println(ByteToStr(p))
 
-        InsertEntry(ByteToStr(p))
-        log.Println(GetEntryAll())
+		var _type string
+		scan.ScanJSON(strings.NewReader(ByteToStr(p)), "/type", &_type)
 
-    }
+		log.Println("type")
+		log.Println(_type)
+
+		if reflect.DeepEqual(_type, "new") {
+			InsertEntry(ByteToStr(p))
+		}
+
+		log.Println(GetEntryAll())
+
+	}
 
 }
