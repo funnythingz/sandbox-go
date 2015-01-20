8723c6683f81832c24652fac317d413518e8bf4c
funnythingz <funnythingz69+github@gmail.com>
2015-01-20 15:46:27 +0900

    renamed

--- gin-gonic/demo.go
+++ gin-gonic/demo.go
@@ -3,19 +3,19 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	router := gin.Default()
+	r := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.String(200, "hello world")
 	})
 
-	router.GET("/json", func(c *gin.Context) {
+	r.GET("/json", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "hey", "status": 200})
 	})
 
-	router.GET("/xml", func(c *gin.Context) {
+	r.GET("/xml", func(c *gin.Context) {
 		c.XML(200, gin.H{"message": "hey", "status": 200})
 	})
 
-	router.Run(":8080")
+	r.Run(":8080")
 }
