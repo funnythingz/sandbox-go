5d31e907a77afcf6a427d0555d249cd054857b6c
funnythingz <funnythingz69+github@gmail.com>
2015-06-07 01:08:15 +0900

    removed not use line

--- closures.go
+++ closures.go
@@ -3,14 +3,12 @@ package main
 import "fmt"
 
 func sayMessage() func(string) string {
-
 	return func(message string) string {
 		return message
 	}
 }
 
 func main() {
-
 	say := sayMessage()
 	fmt.Println(say("test"))
 }
