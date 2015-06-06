19c0920239496ad86e4080c069bae554d3719b67
funnythingz <funnythingz69+github@gmail.com>
2015-06-06 18:30:28 +0900

    try map

---
+++ map.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    values := map[string]string{
+        "name": "unko",
+        "id": "20",
+        }
+    for key, val := range values {
+        fmt.Println(key, val)
+        }
+}
