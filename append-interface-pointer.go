e9711c4ee01b06b2dabaf6fd43bc3b632caa8d4d
funnythingz <funnythingz69+github@gmail.com>
2015-06-09 14:12:33 +0900

    append interface slice pointer

---
+++ append-interface-pointer.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Entity struct {
+	Id int
+}
+
+func main() {
+	var list []interface{}
+
+	hey(&list)
+
+	fmt.Println(list)
+}
+
+func hey(list *[]interface{}) {
+	hoge := Entity{Id: 1}
+
+	*list = append(*list, hoge)
+
+}
