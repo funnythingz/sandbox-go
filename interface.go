7760d6d8c5dd848fd9b9b86b510631c5bd33d78d
funnythingz <funnythingz69+github@gmail.com>
2015-06-19 00:20:29 +0900

    Accessor

--- interface.go
+++ interface.go
@@ -6,21 +6,32 @@ import (
 
 func main() {
 
-	var animals []interface{}
+	var animals []Animal
 
-	animals = append(animals, Dog{Name: "pochi"})
-	animals = append(animals, Cat{Name: "mike"})
+	animals = append(animals, &Dog{Name: "pochi"})
+	animals = append(animals, &Cat{Name: "mike"})
 
-	pp.Println(animals)
+	for _, animal := range animals {
+		pp.Println(animal.GetName())
+	}
 }
 
 type Animal interface {
+	GetName() string
 }
 
 type Dog struct {
 	Name string
 }
 
+func (a *Dog) GetName() string {
+	return a.Name
+}
+
 type Cat struct {
 	Name string
+}
+
+func (a *Cat) GetName() string {
+	return a.Name
 }
