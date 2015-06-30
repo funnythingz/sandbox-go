ccdafe3b9e6d4af6fd17d3591c9fb8bb4dcc96f1
funnythingz <funnythingz69+github@gmail.com>
2015-07-01 00:36:22 +0900

    Fixed map

--- map.go
+++ map.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 )
 
+type Human struct {
+	Name string
+}
+
+func (h *Human) SayName() string {
+	return h.Name
+}
+
 func main() {
-	values := map[string]string{
-		"name": "unko",
-		"id":   "20",
+	values := map[string]interface{}{
+		"name":  "unko",
+		"id":    20,
+		"human": Human{Name: "hehey"},
 	}
 	for key, val := range values {
 		fmt.Println(key, val)
 	}
+
+	human := values["human"].(Human)
+	fmt.Println(human.SayName())
 }
